e4cc598028e1356adf6edbcc602d292917dbcd13
ryu <ryu000407@gmail.com>
2022-02-18 05:18:24 +0900

    operators

---
+++ operators.lua
@@ -0,0 +1,22 @@
+local a, b
+
+a = 10
+b = 20
+
+print("a + b:", a + b)
+print("a - b:", a - b)
+print("a * b:", a * b)
+print("a / b:", a / b)
+print("a % b:", a % b)
+print("a ^ b:", a ^ b)
+print("-a:", -a)
+
+print("a == b:", a == b)
+print("a ~= b:", a ~= b)
+print("a < b:", a < b)
+print("a > b:", a > b)
+print("a <= b:", a <= b)
+print("a >= b:", a >= b)
+
+print("a and b:", a and b)
+print("a or b:", a or b)
